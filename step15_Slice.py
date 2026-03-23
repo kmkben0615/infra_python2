0fc57cc1ee196900b3739d9ba2f3d903e81f5195
kmkben0615 <kmkben307@gmail.com>
2026-03-23 10:09:30 +0900

    Step15추가함

---
+++ step15_Slice.py
@@ -0,0 +1,15 @@
+#Step15_slice.py
+
+nums = [10,20,30,40,50]
+       #0  1  2  3  4
+print(nums[1:])
+print(nums[2:])
+print(nums[3:])
+
+print("----------------")
+print(nums[:3])#3번쨰 인덱스 미만
+print(nums[:2])#
+print(nums[:1])
+
+print("----------------")
+print(nums[1:4])#인덱스1 이상 4 미만 즉 30,40
